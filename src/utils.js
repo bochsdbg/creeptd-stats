5e560fc1f91e93c7fc1138845400fa5aab4b2cd5
NEb0 <onlyforthesky@gmail.com>
2018-08-22 19:04:57 +0300

    Fix firefox zooming and normalization of wheelDelta

--- src/utils.js
+++ src/utils.js
@@ -206,11 +206,61 @@ export function loadCharts(charts) {
     };
 }
 
+function normalizeWheel(/*object*/ event) /*object*/ {
+    const PIXEL_STEP  = 10;
+    const LINE_HEIGHT = 40;
+    const PAGE_HEIGHT = 800;
+
+    var sX = 0, sY = 0,       // spinX, spinY
+        pX = 0, pY = 0;       // pixelX, pixelY
+  
+    // Legacy
+    if ('detail'      in event) { sY = event.detail; }
+    if ('wheelDelta'  in event) { sY = -event.wheelDelta / 120; }
+    if ('wheelDeltaY' in event) { sY = -event.wheelDeltaY / 120; }
+    if ('wheelDeltaX' in event) { sX = -event.wheelDeltaX / 120; }
+  
+    // side scrolling on FF with DOMMouseScroll
+    if ( 'axis' in event && event.axis === event.HORIZONTAL_AXIS ) {
+      sX = sY;
+      sY = 0;
+    }
+  
+    pX = sX * PIXEL_STEP;
+    pY = sY * PIXEL_STEP;
+  
+    if ('deltaY' in event) { pY = event.deltaY; }
+    if ('deltaX' in event) { pX = event.deltaX; }
+  
+    if ((pX || pY) && event.deltaMode) {
+      if (event.deltaMode == 1) {          // delta in LINE units
+        pX *= LINE_HEIGHT;
+        pY *= LINE_HEIGHT;
+      } else {                             // delta in PAGE units
+        pX *= PAGE_HEIGHT;
+        pY *= PAGE_HEIGHT;
+      }
+    }
+  
+    // Fall-back if spin cannot be determined
+    if (pX && !sX) { sX = (pX < 1) ? -1 : 1; }
+    if (pY && !sY) { sY = (pY < 1) ? -1 : 1; }
+  
+    return { spinX  : sX,
+             spinY  : sY,
+             pixelX : pX,
+             pixelY : pY };
+  }
+
 function scrollV3(event, g, context) {
     // if (lastClickedGraph != g) {
     //   return;
     // }
-    let normal = event.detail ? event.detail * -1 : event.wheelDelta / 40;
+    let delta = normalizeWheel(event);
+    
+    let normal = -2 * delta.spinY;
+    // console.log('DeltaY: ', normal);
+    // console.log('DeltaMode: ', event.deltaMode);
     // For me the normalized value shows 0.075 for one click. If I took
     // that verbatim, it would be a 7.5%.
     let percentage = normal / 50;
@@ -364,7 +414,7 @@ export function createChart(charts, chart_name, elem, user_opts) {
         },
 
         interactionModel: {
-            mousewheel: scrollV3,
+            wheel: scrollV3,
 
             mousedown: function(event, g, context) {
                 context.initializeMouseDown(event, g, context);
